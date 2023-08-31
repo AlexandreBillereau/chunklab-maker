c73375fa3572af57744cddf74b1d9874c862ef4e
alxeandre <alexandre.mr.billerau@gmail.com>
2023-08-31 19:53:06 -0400

    feature : now let see if its work with npm

--- bin.js
+++ bin.js
@@ -4,12 +4,12 @@ const { exec } = require("child_process");
 // Fonction pour créer la structure du projet
 
 async function createAppStructure() {
-  // exec("npm install next@latest react@latest react-dom@latest");
   exec(
     "git clone https://github.com/AlexandreBillereau/chunklab-maker.git",
     (err) => {
       try {
-        fs.cpSync("./chunklab-maker/chunklab-app", "./");
+        fs.copySync("./chunklab-maker/chunklab-app/", "./");
+        fs.removeSync("./chunklab-maker/");
       } catch (err) {
         console.error("Une erreur est survenue :", err);
       }
