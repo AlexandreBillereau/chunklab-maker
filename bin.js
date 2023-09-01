e40e07b9ac370b6212349efb94f971077e8c1775
alxeandre <alexandre.mr.billerau@gmail.com>
2023-08-31 22:39:54 -0400

    ✨ add loading screen for dependencies installation

--- bin.js
+++ bin.js
@@ -1,26 +1,54 @@
 #!/usr/bin/env node
 const fs = require("fs-extra");
 const { exec } = require("child_process");
+const chalk = require("chalk");
+const emoji = require("node-emoji");
+const ora = require("ora");
+
+const Title =
+  " $$$$$$\\  $$\\                           $$\\             $$\\                $$\\       \n" +
+  "$$  __$$\\ $$ |                          $$ |            $$ |               $$ |      \n" +
+  "$$ /  \\__|$$$$$$$\\  $$\\   $$\\ $$$$$$$\\  $$ |  $$\\       $$ |      $$$$$$\\  $$$$$$$  \n" +
+  "$$ |      $$  __$$\\ $$ |  $$ |$$  __$$\\ $$ | $$  |      $$ |      \\____$$\\ $$  __$$\\ \n" +
+  "$$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |$$$$$$  /       $$ |      $$$$$$$ |$$ |  $$ |\n" +
+  "$$ |  $$\\ $$ |  $$ |$$ |  $$ |$$ |  $$ |$$  _$$<        $$ |     $$  __$$ |$$ |  $$ |\n" +
+  "\\$$$$$$  |$$ |  $$ |\\$$$$$$  |$$ |  $$ |$$ | \\$$\\       $$$$$$$$\\\\$$$$$$$ |$$$$$$$  |\n" +
+  " \\______/ \\__|  \\__| \\______/ \\__|  \\__|\\__|  \\__|      \\________|\\_______|\\_______/ \n";
+
+const loadingbar = ora();
+loadingbar.spinner = "bouncingBar";
 
 // Function to create project struc
 async function createAppStructure() {
+  console.log("\n");
+  console.log(chalk.cyan(Title));
+  console.log(chalk.green("Chunk-app creation"), emoji.emojify("🚀"));
   exec(
     "git clone https://github.com/AlexandreBillereau/chunklab-maker.git",
     (err) => {
+      console.log(chalk.green("Get the code ", emoji.emojify("📦")));
       try {
         fs.copySync("./chunklab-maker/chunklab-app/", "./");
         fs.removeSync("./chunklab-maker/");
 
-        console.log("Install the dependencies: `npm i` : ");
+        console.log(
+          chalk.green(
+            "Install the",
+            chalk.underline("dependencies"),
+            " >>> `npm i`\n"
+          )
+        );
+        loadingbar.start();
         exec("npm i", (error, stdout, stderr) => {
           if (error) {
-            console.error("Error running 'npm i':", error);
+            console.error("Error running npm install : ", chalk.red(error));
             return;
           }
           if (stderr) {
-            console.error("npm i STDERR:", stderr);
+            console.error("npm install STDERR : ", chalk.red(stderr));
           }
-          console.log("npm i STDOUT:", stdout);
+          loadingbar.stopAndPersist({ symbol: emoji.emojify("✅") });
+          console.log(chalk.cyan(stdout));
         });
       } catch (err) {
         console.error("An error from clonning :", err);
